42642201862ac34f36d10d833db07d88c1b3ee07
Rosanne3 <beuk_r@hotmail.com>
2014-12-16 09:22:28 +1000

    Course project 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The below functions are handy if you have to repeat calculations a lot. 
+# Repeated calculations will not be calculcated over and over again, but are rather
+# stored in a different environment and called when nessecary. If you have a big 
+# dataset, it saves a lot of time.
 
-## Write a short comment describing this function
+
+a<-matrix(c(-1, -2, 1, 1), 2, 2)
+
+
+#makeCacheMatrix sets a matrix, get the values of the matrix, 
+#then sets the value of the inversed matrix in a different environment and finally 
+#gets the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
+#This sets the function in an object called 'mydata'
+mydata<-makeCacheMatrix(a)
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve returns an inversed matrix. If the value is already calculated before, 
+# it returns the value of the stored matrix (and displayse "getting cached data"), 
+# if it is a new matrix, it calculates solve(matrix)
+
+cacheSolve<- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+#This prints your inversed matrix
+cacheSolve(mydata)
